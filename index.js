d7f054fb1a60702a37123ec36ac3ba38000629ae
Timothy Tudosa <timtudosa6@gmail.com>
2020-12-08 00:07:55 -0600

    Added other data from API to render

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 const express = require('express');
 const app = express();
 const https = require('https');
+const path = require('path');
 
+
+
+app.use(express.static(path.join(__dirname,'views')));
 const port = 7622;
 
 app.set('view engine','ejs');
+
+app.use(express.static(path.join(__dirname,"public")));
+
+
+convertToFahrenheit = (temperature) => {
+      return (temperature - temperature) * 9/5 + 32;
+}
 
 app.get('/',(req,res)=>{
       https.get('https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=fd46c79dc6499d0f5dbe1742d3c16495',response=>{
         response.on('data',data=>{
           var parsedData = JSON.parse(data);
-          res.write(`<div>
-            <h1>Chicago's Weather</h1><h3>Temperature${parsedData.main.temp}</h3><p>Wind Speed: ${parsedData.wind.speed}</p>
-          <p>Wind Temperature: ${parsedData.wind.deg}</p></div>`);
-          res.send();
+          res.render('view',{image:parsedData.weather.icon,description:parsedData.weather[0].description,temperature:convertToFahrenheit(parsedData.main.temp),windSpeed:parsedData.wind.speed,windTemperature:convertToFahrenheit(parsedData.wind.deg),humidity:parsedData.main.humidity});
     });
 
 });
 });
-
-
-
-
-
-// app.post('/getCall',(req,res)=>{
-//   https.get('http://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=fd46c79dc6499d0f5dbe1742d3c16495',response=>{
-//     response.on('data',data=>{
-//       console.log(JSON.parse(data));
-//     });
-//   });
-// });
 
 app.listen(port,err=>{
   try {
