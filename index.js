bb26e05153f1f5ac6d5bdbe8197610d7e07f7d9a
Timothy Tudosa <timtudosa6@gmail.com>
2020-12-23 14:06:35 -0600

    Had wrong formual to convert Kelvin temperature to Fahrenheit. I fixed the formula and now it should be good to go.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 
 app.use(express.static(path.join(__dirname,'views')));
+
 const port = process.env.PORT || 7622;
 
 app.set('view engine','ejs');
@@ -13,8 +14,8 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,"public")));
 
 
-convertToFahrenheit = (temperature) => {
-      return (temperature - temperature) * 9/5 + 32;//formula to automate conversion of temperatures from Kelvin to Fahrenheit(The USA way!)
+const convertToFahrenheit = (temperature) => {
+      return 9/5 * (temperature - 273)+ 32;//formula to automate conversion of temperatures from Kelvin to Fahrenheit(The USA way!)
 }
 
 app.get('/',(req,res)=>{
@@ -23,6 +24,9 @@ app.get('/',(req,res)=>{
           var parsedData = JSON.parse(data);
           res.render('view',{image:parsedData.weather.icon,description:parsedData.weather[0].description,temperature:convertToFahrenheit(parsedData.main.temp),windSpeed:parsedData.wind.speed,windTemperature:convertToFahrenheit(parsedData.wind.deg),humidity:parsedData.main.humidity});
     });
+
+    //Temperature comes in  kelvin e.g. 175.34. Need to convert this value to fahrenheit and round to the nearest tenth. 
+    //if the digit right after the decimal point is 5 or higher round up if the digit right after the decimal is 5 or lower the number will remain constant
 
 });
 });
